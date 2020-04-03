5852b134640d959f5cd1c9078c0c0f419223a69f
ninjee <yninjbadgar@gmail.com>
2020-04-03 16:46:02 +0800

    Toglogchiin eeljiig solidog bolgono.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдгэлэе
-var activePlayer = 1;
+var activePlayer = 0;
 //  Тоглогчын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 // Тоглогчын ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
@@ -17,9 +17,40 @@ var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 // console.log("Шоо: " + diceNumber);
 
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1-6 дотор санамсаргүй нэг утга гаргаж авна.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //   Шооны зургийг веб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  //   Буусан санамсаргүй тоонд харгалзах шооны зургийг веб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
-  //   alert("SHoo buulaa: " + diceNumber);
+
+  if (diceNumber !== 1) {
+    // 1 ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //   if(activePlayer === 0 ){
+    //       activePlayer = 1;
+    //   }else{ activePlayer = 0; }   // Энэ арга болон доод талын арга яг адилхан
+
+    // Энэ тоглогчын цуглуулсан оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    // Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ.
+    // Хэрэв идэвхитэй тоглогч нь 0 байтал 1 болго.
+    // Үгүй бол тоглогчыг 0 болго
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // Улаан цэгийг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active"); // end remove bolon add ashiglaj bolno gehde Toggle function ni  baiwal ustgadag baihgui bol tuhain classiig nemdeg uchir ilvv amar gesn vg.
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // Шоог түр алга болгох 1 үед
+    diceDom.style.display = "none";
+  }
 });
